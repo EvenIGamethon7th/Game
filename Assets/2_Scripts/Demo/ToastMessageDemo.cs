66fb3c36dfea7dac09e0571f436070b442773f62
zhaqj12 <zhaqj12@naver.com>
2024-06-17 23:03:46 +0900

    Dot tween Add And ToastMessage Demo Add

---
+++ Assets/2_Scripts/Demo/ToastMessageDemo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace _2_Scripts.Demo
+{
+    public class ToastMessageDemo : MonoBehaviour
+    {
+        private void Start()
+        {
+            UI_Toast_Manager.Instance.Activate_WithLcz_Func(TableDataKey_C.Localize_Lo_Test, () =>
+            {
+                UI_Toast_Manager.Instance.Activate_WithContent_Func("끝남");
+            });
+            
+        }
+    }
+}
---
+++ Assets/2_Scripts/Library_C/UI_Toast_Manager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Cargold;
+
+public class UI_Toast_Manager : Cargold.UI.UI_Toast_Manager
+{
+    public static new UI_Toast_Manager Instance;
+
+    public override void Init_Func(int _layer)
+    {
+        base.Init_Func(_layer);
+
+        if (_layer == 0)
+        {
+            Instance = this;
+        }
+    }
+
+    protected override string GetLczStr_AdsFail_Func() => "!광고 실패...";
+    protected override string GetLczStr_BtnContinuousAlarm_Func() => "!버튼을 꾹 눌러도 됩니다.";
+    protected override string GetLczStr_PurchaseFail_Func() => "!결제 실패...";
+    protected override string GetLczStr_AdsRemove_Func() => "!광고 제거";
+    protected override string GetLczStr_SameLangTypeSelected_Func() => "!동일한 언어를 선택했습니다.";
+}

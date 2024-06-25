a78a82297add40a87b2c90aabc76a74ccc36c42b
황경훈 <zhaqj12@naver.com>
2024-06-25 16:32:48 +0900

    #17 필드 그리드 시스템 구현 ( 배치 가능한 셀 Indicator 표시 중 )

---
+++ Assets/2_Scripts/Controller/MapController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+/// 테스트용 맵 컨트롤러
+namespace _2_Scripts.Controller
+{
+    public class MapController : MonoBehaviour
+    {
+        [SerializeField] private Tilemap mMap;
+
+        /// <summary>
+        ///  테스트용 인디케이터
+        /// </summary>
+        [SerializeField] private GameObject mIndicatorPrefab;
+
+        private List<GameObject> mIndicatorList;
+        private void Start()
+        {
+            /* 유니티 에디터 문제로 제대로 cellBounds가 업데이트 되지 않을 때 가 있기 때문에 아래 글 참조. 또는 빈번히 발생하면 UniRx로 셀 사이즈 변경 감지되면 변경할 수 있도록 구독 처리할 생각임
+            https://www.reddit.com/r/Unity2D/comments/daxnk1/whats_wrong_with_tilemapcellbounds_or_me/*/
+            MoveIndicatorGenerate();
+        }
+
+        private void MoveIndicatorGenerate()
+        {
+            BoundsInt bounds = mMap.cellBounds;
+            for (int x = bounds.xMin; x < bounds.xMax; x+= 2)
+            {
+                for (int y = bounds.yMin; y < bounds.yMax; y++)
+                {
+                    Vector3Int cellPosition = new Vector3Int(x, y, 0);
+                    Vector3 worldPosition = mMap.CellToWorld(cellPosition);
+                    Instantiate(mIndicatorPrefab, worldPosition, Quaternion.identity);
+                }
+            }
+        }
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector3Int gridPosition = mMap.WorldToCell(pos);
+
+     
+                TileBase clickedTile = mMap.GetTile(gridPosition);
+                print($"At Pos {gridPosition} " );
+            }
+        }
+    }
+}
---
+++ Assets/2_Scripts/ScriptableObjects/TileData.cs
@@ -0,0 +1,15 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace _2_Scripts.ScriptableObjects
+{
+    [CreateAssetMenu]
+    public class TileData : SerializedScriptableObject
+    {
+        [SerializeField]
+        public TileBase[] Tiles { get; private set; }
+        [SerializeField]
+        public bool IsGround { get; private set; }
+    }
+}

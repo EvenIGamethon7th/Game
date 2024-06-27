e5e957af1486436a6f4a3b8139b93dd07f234083
sansitasan <20161897@kookmin.ac.kr>
2024-06-27 20:38:46 +0900

    Temporary Save

---
+++ Assets/2_Scripts/Game/Map/Indicator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace _2_Scripts.Game.Map
+{
+    public class Indicator : MonoBehaviour
+    {
+        private LineRenderer mLine;
+        private GameObject mUnitMark;
+        private GameObject mDstMark;
+    }
+}

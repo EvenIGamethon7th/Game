1b16cc45f345d506e4d4599f2a3c3d86c30a6d23
황경훈 <zhaqj12@naver.com>
2024-06-27 14:43:45 +0900

    #36 Wave, Stage , Monster 시스템 초기 구현 완료

--- Assets/2_Scripts/Game/Unit/Monster/Monster.cs
+++ Assets/2_Scripts/Game/Unit/Monster/Monster.cs
@@ -6,7 +6,7 @@ namespace _2_Scripts.Game.Monster
 {
     public class Monster : MonoBehaviour
     {
-        [GetComponent] private SpriteRenderer mSprite;
+        [GetComponent] private Animator mAnimator;
         
         private MonsterData mMonsterData;
         public void SpawnMonster(string key)
@@ -14,6 +14,7 @@ namespace _2_Scripts.Game.Monster
             var originData = DataBase_Manager.Instance.GetMonster.GetData_Func(key);
             mMonsterData = global::Utils.DeepCopy(originData);
             //TODO Sprite Change And Animation
+            mAnimator = ResourceManager.Instance.Load<Animator>(originData.addressableKey);
         }
         
     }
--- Assets/2_Scripts/Library_C/FrameWork/StageWave/StageManager.cs
+++ Assets/2_Scripts/Library_C/FrameWork/StageWave/StageManager.cs
@@ -1,7 +1,11 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using _2_Scripts.Game.Monster;
+using _2_Scripts.Utils;
+using Cargold;
 using Cysharp.Threading.Tasks;
+using UniRx;
 using UnityEngine;
 
 public class StageManager : Singleton<StageManager>
@@ -15,7 +19,23 @@ public class StageManager : Singleton<StageManager>
     
     private const float SPAWN_COOL_TIME = 1.0f;
     private const float NEXT_WAVE_TIME = 3.0f;
-    public void StageInit(int stageKey)
+
+
+    /// <summary>
+    ///  테스트용 스테이지 시작 코드
+    /// </summary>
+    /// <exception cref="NotImplementedException"></exception>
+    public void Start()
+    {
+        MessageBroker.Default.Receive<TaskMessage>().Where(message => message.Task == ETaskList.ResourceLoad).Subscribe(
+            _ =>
+            {
+                StageInit(TableDataKey_C.Stage_Stage_0);
+                
+            });
+    }
+
+    public void StageInit(string stageKey)
     {
         mCurrentStageData = DataBase_Manager.Instance.GetStage.GetData_Func(stageKey);
         foreach (var wave in mCurrentStageData.waveList)
@@ -37,13 +57,14 @@ public class StageManager : Singleton<StageManager>
             }
             mCurrentWaveData = mWaveQueue.Dequeue();
             int spawnCount = 0;
-            while (spawnCount >= mCurrentWaveData.spawnCount)
+            while (spawnCount < mCurrentWaveData.spawnCount)
             {
-                Monster monster =  ObjectPoolManager.Instance.CreatePoolingObject(mCurrentWaveData.monsterKey,mSpawnPoint).GetComponent<Monster>();
+                Monster monster =  ObjectPoolManager.Instance.CreatePoolingObject("Monster",mSpawnPoint).GetComponent<Monster>();
                 monster.SpawnMonster(mCurrentWaveData.monsterKey);
                 await UniTask.WaitForSeconds(SPAWN_COOL_TIME);
             }
             await UniTask.WaitForSeconds(NEXT_WAVE_TIME);
+            Debug_C.Log_Func($"다음 Wave 시작 {mCurrentWaveData.Key}");
         }
         // ReSharper disable once FunctionNeverReturns
     }

68b33e97a0ff9fbd181023597eb9512b5c6d4eca
황경훈 <zhaqj12@naver.com>
2024-07-01 12:00:12 +0900

    #79 User HP Bar 구현

--- Assets/2_Scripts/Library_C/FrameWork/GameSystem_Manager.cs
+++ Assets/2_Scripts/Library_C/FrameWork/GameSystem_Manager.cs
@@ -1,9 +1,11 @@
+using _2_Scripts.Utils;
 using Cargold.FrameWork;
 using UnityEngine;
 using Cargold.Infinite;
-
 public class GameSystem_Manager : Cargold.FrameWork.GameSystem_Manager
 {
+
+
     protected override void Init_Func()
     {
         base.Init_Func();
---
+++ Assets/2_Scripts/Library_C/GameManager.cs
@@ -0,0 +1,20 @@
+﻿
+    //  HP , 재화 , 씬 이동 등 여러가지 관리 목적
+
+    using System;
+    using _2_Scripts.Utils;
+    using UniRx;
+
+    public class GameManager : Singleton<GameManager>
+    {
+        
+        public float UserHp { get; private set; } = 100;
+        private void Start()
+        {
+            MessageBroker.Default.Receive<GameMessage<float>>().Where(message => message.Message == EGameMessage.PlayerDamage)
+                .Subscribe(message =>
+                {
+                    UserHp -= message.Value;
+                });
+        }
+    }

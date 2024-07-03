3b012af4517f6ad1ccc9f93dc293db62affc2611
황경훈 <zhaqj12@naver.com>
2024-07-03 12:59:01 +0900

    #86 연출 효과 Fix & Puff 효과 추가

---
+++ Assets/2_Scripts/UI/UI_PuffDamage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using _2_Scripts.Utils;
+using AssetKits.ParticleImage;
+using DG.Tweening;
+using UniRx;
+using UnityEngine;
+
+namespace _2_Scripts.UI
+{
+    public class UI_PuffDamage : MonoBehaviour
+    {       
+        private ParticleImage mPaticleImage;
+        private void Start()
+        {
+            mPaticleImage = GetComponent<ParticleImage>();
+            MessageBroker.Default.Receive<GameMessage<float>>()
+                .Where(message => message.Message == EGameMessage.PlayerDamage)
+                .Subscribe(message =>
+                {
+                    mPaticleImage.Play();
+                }).AddTo(this);
+        }
+    }
+}

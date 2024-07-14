a45d65b304eb1aa282b667d706dfa8f734c9b342
zhaqj12 <zhaqj12@naver.com>
2024-07-14 22:41:26 +0900

    #106 파티클 애니메이션 끝날 시 GameObject Disable ( AnimatioanToEndDisable Component )

---
+++ Assets/2_Scripts/Utils/Components/AnimationEndToDisable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace _2_Scripts.Utils.Components
+{
+    public class AnimationEndToDisable : MonoBehaviour
+    {
+        private Animator mAnimator;
+        private bool mIsPlaying = false;
+        private void Awake()
+        {
+            mAnimator = GetComponent<Animator>();
+        }
+
+        private void OnEnable()
+        {
+             mAnimator.Play(0,-1,0);
+             mIsPlaying = true;
+        }
+        private void Update()
+        {
+            if (mIsPlaying && mAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                mIsPlaying = false;
+                if (transform.parent != null)
+                {
+                    transform.parent.gameObject.SetActive(false);
+                }
+                else
+                {
+                    gameObject.SetActive(false);
+                }
+         
+            }
+            
+            
+        }
+    }
+}

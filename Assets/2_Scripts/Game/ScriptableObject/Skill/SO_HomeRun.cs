10054cca262c9315a875ce0cc1cba3da86719349
sansitasan <20161897@kookmin.ac.kr>
2024-07-16 05:34:53 +0900

    Just Save

---
+++ Assets/2_Scripts/Game/ScriptableObject/Skill/SO_HomeRun.cs
@@ -0,0 +1,41 @@
+using _2_Scripts.Game.Unit;
+using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace _2_Scripts.Game.ScriptableObject.Skill
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/Skill/HomeRun")]
+    public class SO_HomeRun : Skill
+    {
+        [SerializeField]
+        [Title("데미지 증감 퍼센트")]
+        private float mPercentDamage;
+
+        public override bool CastAttack(Transform ownerTransform, CharacterData ownerData, Action<Monster.Monster[]> beforeDamage = null, Action<Monster.Monster> afterDamage = null)
+        {
+            var detectingTargets = Physics2D.OverlapCircleAll(ownerTransform.position, ownerData.range, TargetLayer);
+            if (detectingTargets.Length == 0)
+            {
+                return false;
+            }
+
+            var monsterArray = detectingTargets
+                .Select(collider => collider.GetComponent<Monster.Monster>())
+                .Where(monster => monster != null)
+                .ToArray();
+
+            beforeDamage?.Invoke(monsterArray);
+            CastEffectPlay(ownerTransform.position);
+            var attacker = ownerTransform.GetComponent<CUnit>();
+            attacker.SetFlipUnit(detectingTargets[0].transform);
+
+            var monster = monsterArray.Where(monster => !monster.IsDead).FirstOrDefault();
+            if (monster == null) return true;
+            return true;
+        }
+    }
+}

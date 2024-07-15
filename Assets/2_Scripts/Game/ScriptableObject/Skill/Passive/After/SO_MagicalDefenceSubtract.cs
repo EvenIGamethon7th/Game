3169a6c70889a556239b0c6d8a92dd71ace5e121
zhaqj12 <zhaqj12@naver.com>
2024-07-15 23:04:53 +0900

    #106 엔젤 아처 구현

---
+++ Assets/2_Scripts/Game/ScriptableObject/Skill/Passive/After/SO_MagicalDefenceSubtract.cs
@@ -0,0 +1,18 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _2_Scripts.Game.ScriptableObject.Skill.Passive
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/PassiveSkill/MagicDefenceSubtract")]
+    public class SO_MagicalDefenceSubtract : AfterPassive
+    {
+        [Title("차감할 마법 방어력 퍼센트")] [SerializeField] private float mPercent; 
+        public override void AfterDamage(Monster.Monster monsters)
+        {
+            if(monsters.DefenceFlag)
+                return;
+            monsters.GetMonsterData.mdef -= monsters.GetMonsterData.mdef * (mPercent * 0.01f);
+            monsters.DefenceFlag = true;
+        }
+    }
+}

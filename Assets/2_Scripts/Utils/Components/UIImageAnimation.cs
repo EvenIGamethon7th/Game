07a486875f14d3c03e208c6dd0b1b676a707e3ab
zhaqj12 <zhaqj12@naver.com>
2024-07-18 04:01:34 +0900

    #117 보물상자 어셋 등록 및 파티클 연출 완료

---
+++ Assets/2_Scripts/Utils/Components/UIImageAnimation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Cargold;
+using Sirenix.OdinInspector;
+using UniRx;
+
+namespace _2_Scripts.Utils.Components
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class UIImageAnimation : SerializedMonoBehaviour
+    {
+
+        [SerializeField] private Dictionary<string, Sprite[]> mAnimDic;
+        private Image mImage;
+
+        private void Start()
+        {
+            mImage = GetComponent<Image>();
+        }
+
+        public void PlayAnimation(string key, float speed,Action onCompleteCallback = null)
+        {
+            if(!mAnimDic.ContainsKey(key))
+            {
+                Debug.LogError("Not Found Animation Key!");
+            }
+
+            int idx = 0;
+            Observable.Interval(System.TimeSpan.FromSeconds(speed))
+                .TakeWhile(_=> idx < mAnimDic[key].Length)
+                .Subscribe(_ =>
+                    {
+                        mImage.sprite = mAnimDic[key][idx++];
+                    }, 
+                    () =>
+                    {
+                        idx = 0;
+                        onCompleteCallback?.Invoke();
+                    })
+                .AddTo(this);
+        }
+
+    }
+}

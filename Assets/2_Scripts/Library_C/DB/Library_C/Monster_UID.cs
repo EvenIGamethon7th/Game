90eed399d64eeac2d0e82ded54bf99e815dfde6c
zhaqj12 <zhaqj12@naver.com>
2024-07-18 17:29:57 +0900

    #117 상자뽑기 UI 구현 완

--- Assets/2_Scripts/Library_C/DB/Library_C/Monster_UID.cs
+++ Assets/2_Scripts/Library_C/DB/Library_C/Monster_UID.cs
@@ -17,4 +17,22 @@ public static partial class TableDataKey_C
     public const string Monster_Monster_13 = "Monster_13";
     public const string Monster_Monster_14 = "Monster_14";
     public const string Monster_Monster_15 = "Monster_15";
+    public const string Monster_Monster_16 = "Monster_16";
+    public const string Monster_Monster_17 = "Monster_17";
+    public const string Monster_Monster_18 = "Monster_18";
+    public const string Monster_Monster_19 = "Monster_19";
+    public const string Monster_Monster_20 = "Monster_20";
+    public const string Monster_Monster_21 = "Monster_21";
+    public const string Monster_Monster_22 = "Monster_22";
+    public const string Monster_Monster_23 = "Monster_23";
+    public const string Monster_Monster_24 = "Monster_24";
+    public const string Monster_Monster_25 = "Monster_25";
+    public const string Monster_Monster_26 = "Monster_26";
+    public const string Monster_Monster_27 = "Monster_27";
+    public const string Monster_Monster_28 = "Monster_28";
+    public const string Monster_Monster_29 = "Monster_29";
+    public const string Monster_Monster_30 = "Monster_30";
+    public const string Monster_Monster_31 = "Monster_31";
+    public const string Monster_Monster_32 = "Monster_32";
+    public const string Monster_Monster_33 = "Monster_33";
 }

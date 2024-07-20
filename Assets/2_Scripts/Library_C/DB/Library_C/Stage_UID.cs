7d206f83d02c07a184a513c39441af924dc71e3b
zhaqj12 <zhaqj12@naver.com>
2024-07-20 23:16:59 +0900

    빌드 버그 및 ExpBar 이름 버그 수정

--- Assets/2_Scripts/Library_C/DB/Library_C/Stage_UID.cs
+++ Assets/2_Scripts/Library_C/DB/Library_C/Stage_UID.cs
@@ -2,4 +2,23 @@
 public static partial class TableDataKey_C
 {
     public const string Stage_Stage_0 = "Stage_0";
+    public const string Stage_Stage_1 = "Stage_1";
+    public const string Stage_Stage_2 = "Stage_2";
+    public const string Stage_Stage_3 = "Stage_3";
+    public const string Stage_Stage_4 = "Stage_4";
+    public const string Stage_Stage_5 = "Stage_5";
+    public const string Stage_Stage_6 = "Stage_6";
+    public const string Stage_Stage_7 = "Stage_7";
+    public const string Stage_Stage_8 = "Stage_8";
+    public const string Stage_Stage_9 = "Stage_9";
+    public const string Stage_Stage_10 = "Stage_10";
+    public const string Stage_Stage_11 = "Stage_11";
+    public const string Stage_Stage_12 = "Stage_12";
+    public const string Stage_Stage_13 = "Stage_13";
+    public const string Stage_Stage_14 = "Stage_14";
+    public const string Stage_Stage_15 = "Stage_15";
+    public const string Stage_Stage_16 = "Stage_16";
+    public const string Stage_Stage_17 = "Stage_17";
+    public const string Stage_Stage_18 = "Stage_18";
+    public const string Stage_Stage_19 = "Stage_19";
 }

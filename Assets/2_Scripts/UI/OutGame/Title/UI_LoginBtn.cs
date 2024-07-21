00c711187616b7f729a78febedf8fc2efd820882
zhaqj12 <zhaqj12@naver.com>
2024-07-21 23:55:58 +0900

    백엔드 매니저 추가

---
+++ Assets/2_Scripts/UI/OutGame/Title/UI_LoginBtn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Cargold.FrameWork.BackEnd;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+
+namespace _2_Scripts.UI.OutGame.Title
+{
+    public class UI_LoginBtn : MonoBehaviour
+    {
+        private Button mButton;
+
+        private void Start()
+        {
+            mButton = GetComponent<Button>();
+            mButton.onClick.AddListener(OnClickLoginBtn);   
+        }
+
+        private void OnClickLoginBtn()
+        {
+            mButton.interactable = false;
+            BackEndManager.Instance.OnLogin(() =>
+            {
+                SceneManager.LoadScene("1_Scenes/Main");
+            });
+        }
+    }
+}

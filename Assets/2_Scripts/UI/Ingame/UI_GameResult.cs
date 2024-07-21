b1579bf337a96aab40b243e0edf8e0110e49918f
zhaqj12 <zhaqj12@naver.com>
2024-07-22 06:12:41 +0900

    #78 승리 패배 Prefab 추가

---
+++ Assets/2_Scripts/UI/Ingame/UI_GameResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace _2_Scripts.UI.Ingame
+{
+    public class UI_GameResult : MonoBehaviour
+    {
+        [SerializeField] private GameObject mVictory;
+        [SerializeField] private GameObject mDefeat;
+
+        private void Start()
+        {
+            
+        }
+    }
+}
---
+++ Assets/2_Scripts/UI/Ingame/UI_Victory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using Cargold;
+using Coffee.UIExtensions;
+using DG.Tweening;
+using UnityEngine;
+
+namespace _2_Scripts.UI.Ingame
+{
+    public class UI_Victory : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform mVictoryPanel;
+
+        [SerializeField] private GameObject[] mStarts;
+
+        [SerializeField]
+        private UIParticle mParticle;
+
+        private void Start()
+        {
+            Vector2 originPos = mVictoryPanel.anchoredPosition;
+            mVictoryPanel.DOAnchorPosY(originPos.y + 10,1f).SetEase(Ease.InOutQuad).SetLoops(-1,LoopType.Yoyo);
+            StartCoroutine(StartAnimationCoroutine());
+        }
+
+        IEnumerator StartAnimationCoroutine()
+        {
+            int rank = RankCalculator(GameManager.Instance.UserHp.Value);
+            for (int i = 0; i < rank; i++)
+            {
+                Tween_C.OnPunch_Func(mStarts[i]);
+                yield return new WaitForSeconds(0.5f);
+                mParticle.rectTransform.position = mStarts[i].transform.position;
+                mParticle.Play();
+                foreach (Transform child in mStarts[i].transform)
+                {
+                    child.gameObject.SetActive(true);
+                }
+            }
+        }
+        
+        private  int RankCalculator(float hp)
+        {
+            return hp switch
+            {
+                >= 70 => 3,
+                >= 40 => 2,
+                _ => 1,
+            };
+        }
+    }
+}

d947662377d081e1b48aa364a58f84135b7ec9a2
zhaqj12 <zhaqj12@naver.com>
2024-07-26 02:19:25 +0900

    select Indicator fix

--- Assets/2_Scripts/UI/OutGame/Lobby/UI_StageIndicator.cs
+++ Assets/2_Scripts/UI/OutGame/Lobby/UI_StageIndicator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using _2_Scripts.Game.Chapter;
 using _2_Scripts.Utils;
+using DG.Tweening;
 using UniRx;
 using UnityEngine;
 
@@ -10,10 +11,26 @@ namespace _2_Scripts.UI.OutGame.Lobby
     {
         [SerializeField]
         private RectTransform mIndicator;
+        private Tween moveTween;
+
+        public void Start()
+        {
+            StartMoveAnimation();
+        }
+
+        private void StartMoveAnimation()
+        {
+            moveTween?.Kill(); // 기존 애니메이션이 있으면 종료
+            moveTween = mIndicator.DOAnchorPosY(mIndicator.anchoredPosition.y + 50, 0.5f)
+                .SetEase(Ease.InOutSine)
+                .SetLoops(-1, LoopType.Yoyo);
+        }
+
         public void OnChange(Chapter chapter)
         {
             mIndicator.gameObject.SetActive(chapter.IsLocked);
-            transform.position = chapter.LastClearStage.GetRectTransform().position;
+            mIndicator.anchoredPosition = chapter.LastClearStage.GetRectTransform().anchoredPosition;
+            StartMoveAnimation(); // 위치가 변경되었으므로 애니메이션을 재시작
         }
     }
 }

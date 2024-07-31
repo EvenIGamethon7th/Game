b20060ff454d020f8b40258fd1fa931f46869405
zhaqj12 <zhaqj12@naver.com>
2024-08-01 01:55:40 +0900

    UI select Item

---
+++ Assets/2_Scripts/Game/BackEndData/Enchant/CharacterEnchantData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2_Scripts.Game.BackEndData.Enchant
+{
+    [Serializable]
+    public class CharacterEnchantData
+    {
+        public string CharacterKey;
+        public int EnchantLevel;
+        public bool isMaxEnchant;
+        
+        public CharacterEnchantData(string characterKey, int enchantLevel, bool isMaxEnchant)
+        {
+            CharacterKey = characterKey;
+            EnchantLevel = enchantLevel;
+            this.isMaxEnchant = isMaxEnchant;
+        }
+
+        //돈 계산은 알아서하라하고 여긴 Level만 관리
+        public void Enchant()
+        {
+            if(isMaxEnchant == false)
+                EnchantLevel++;
+            
+        }
+    }
+}

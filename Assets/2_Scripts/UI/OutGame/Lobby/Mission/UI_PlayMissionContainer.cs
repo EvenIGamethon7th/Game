cf104157fbb3f636c39d0e67df972a2dbd2581a6
zhaqj12 <zhaqj12@naver.com>
2024-08-12 21:48:21 +0900

    PlayMission UI Completed

--- Assets/2_Scripts/UI/OutGame/Lobby/Mission/UI_PlayMissionContainer.cs
+++ Assets/2_Scripts/UI/OutGame/Lobby/Mission/UI_PlayMissionContainer.cs
@@ -1,9 +1,24 @@
 ﻿using Sirenix.OdinInspector;
+using System;
+using UnityEngine;
 
 namespace _2_Scripts.UI.OutGame.Lobby
 {
     public class UI_PlayMissionContainer : SerializedMonoBehaviour
     {
-        
+        [SerializeField]
+        private UI_PlayMissionItem mPlayMissionItemPrefab;
+
+        [SerializeField] private SO_PlayMissionTable mMissionTable;
+
+        private void Start()
+        {
+            foreach (var soPlayMission in mMissionTable.PlayMissionTable)
+            {
+                soPlayMission.Value.InitMission(soPlayMission.Key);
+                var playMissionItem = Instantiate(mPlayMissionItemPrefab, transform);
+                playMissionItem.InitItem(soPlayMission.Key, soPlayMission.Value);
+            }
+        }
     }
 }
--- Assets/2_Scripts/UI/OutGame/Lobby/Mission/UI_PlayMissionItem.cs
+++ Assets/2_Scripts/UI/OutGame/Lobby/Mission/UI_PlayMissionItem.cs
@@ -1,9 +1,42 @@
-﻿using UnityEngine;
+﻿using Cargold.FrameWork.BackEnd;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace _2_Scripts.UI.OutGame.Lobby
 {
-    public class UI_PlayMissionItem : MonoBehaviour
+    public class UI_PlayMissionItem : SerializedMonoBehaviour
     {
+        enum ETextType
+        {
+            SubTitle,
+            Title,
+            Star,
+            RewardAmount,
+            ButtonText
+        }
+        [SerializeField] private Dictionary<ETextType,TextMeshProUGUI> mTexts;
+        [SerializeField] private Slider mSlider;
+        [SerializeField] private Button mButton;
+        [SerializeField] private Image mRewardItemImage;
+
+        private PlayMission mPlayMission;
+        public void InitItem(string missionKey,SO_PlayMission playMission)
+        {
+            mPlayMission = BackEndManager.Instance.UserPlayMission[missionKey];
+            mTexts[ETextType.SubTitle].text = mPlayMission.MissionSubName;
+            mTexts[ETextType.Title].text = mPlayMission.MissionName;
+            mTexts[ETextType.RewardAmount].text = $"{playMission.Amount}";
+            mRewardItemImage.sprite = playMission.ItemKey.GetData.Icon;
+            StarTextChange(playMission.GetCurrentProgress(),playMission.GetMaxProgress());
+        }
         
+        private void StarTextChange(int currentStar, int maxStar)
+        {
+            mSlider.value = currentStar/(float)maxStar;
+            mTexts[ETextType.Star].text = $"{currentStar}/{maxStar}";
+        }
     }
 }

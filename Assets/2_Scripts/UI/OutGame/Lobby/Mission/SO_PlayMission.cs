917f78b1abf3ab43a669c8469233b078a780c30c
zhaqj12 <zhaqj12@naver.com>
2024-08-12 20:36:33 +0900

    PlayMissionData Completed

---
+++ Assets/2_Scripts/UI/OutGame/Lobby/Mission/SO_PlayMission.cs
@@ -0,0 +1,49 @@
+﻿using _2_Scripts.Utils;
+using _2_Scripts.Utils.Components;
+using Cargold.FrameWork.BackEnd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace _2_Scripts.UI.OutGame.Lobby
+{
+    [Serializable]
+    public class SO_PlayMission
+    {
+        [SerializeField]
+       public List<MissionClearCondition> ClearConditions { get; private set; }
+       [SerializeField]
+       public ItemKey ItemKey { get; private set; }
+       [SerializeField]
+       public int Amount { get; private set; }
+       [SerializeField]
+       public MissionReward ItemAcquisition;
+       private PlayMission Mission;
+
+       public int GetCurrentProgress()
+       {
+           return ClearConditions.Sum(x => x.GetCurrentProgress());
+       }
+       public int GetMaxProgress()
+       {
+           return ClearConditions.Sum(x => x.GetMaxProgress());
+       }
+       
+       public void InitMission(string missionKey)
+       {
+           Mission = BackEndManager.Instance.UserPlayMission[missionKey];
+       }
+       
+       public bool ShouldGrantReward()
+       {
+           if(Mission.IsClear || ClearConditions.Any(x => x.IsClear() == false))
+           {
+               return false;
+           }
+           Mission.IsClear = true;
+           ItemAcquisition.ItemAcquisition.AcquireItem(ItemKey,Amount);
+           return true;
+       } 
+    }
+}
---
+++ Assets/2_Scripts/UI/OutGame/Lobby/Mission/SO_PlayMissionTable.cs
@@ -0,0 +1,15 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _2_Scripts.UI.OutGame.Lobby
+{
+    [CreateAssetMenu(fileName = "SO_PlayMissionTable", menuName = "ScriptableObject/PlayMissionTable")]
+    public class SO_PlayMissionTable : SerializedScriptableObject
+    {
+        [SerializeField]
+        public Dictionary<string,SO_PlayMission> PlayMissionTable { get; private set; }
+       
+        
+    }
+}

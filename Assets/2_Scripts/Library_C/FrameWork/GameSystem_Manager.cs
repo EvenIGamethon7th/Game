7b0d7e442c6677def7173c9010e039c866ed444f
황경훈 <zhaqj12@naver.com>
2024-08-22 02:35:43 +0900

    change introducePack

--- Assets/2_Scripts/Library_C/FrameWork/GameSystem_Manager.cs
+++ Assets/2_Scripts/Library_C/FrameWork/GameSystem_Manager.cs
@@ -9,8 +9,8 @@ public class GameSystem_Manager : Cargold.FrameWork.GameSystem_Manager
     protected override void Init_Func()
     {
         base.Init_Func();
-
         // 프로젝트가 시작되면 가장 먼저 호출되는 곳입니다.
+       
     }
 
 

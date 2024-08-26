aca83d6f6cea8187b79bcd149e81d12a73a9bd47
황경훈 <zhaqj12@naver.com>
2024-08-27 05:11:29 +0900

    reroll

--- Assets/2_Scripts/UI/Ingame/Summon/UI_RerollButton.cs
+++ Assets/2_Scripts/UI/Ingame/Summon/UI_RerollButton.cs
@@ -1,3 +1,4 @@
+using _2_Scripts.Game.BackEndData.Stage;
 using _2_Scripts.Game.Sound;
 using Cargold;
 using Cysharp.Threading.Tasks;
@@ -17,15 +18,16 @@ namespace _2_Scripts.UI
         [SerializeField]
         private UI_SummonButton[] mSummonButtons;
 
-        private const int REROOL_COST = 20;
+        private int mRerollCost = 20;
 
         [SerializeField] private UI_LockButton mLockButton;
         private void Start()
         {
+            
             mButton.onClick.AddListener(OnClickReRollBtn);
             IngameDataManager.Instance.Subscribe(this, IngameDataManager.EDataType.Gold, gold =>
             {
-                if (gold < REROOL_COST)
+                if (gold < mRerollCost)
                 {
                     mButton.interactable = false;
                 }
@@ -44,7 +46,11 @@ namespace _2_Scripts.UI
                 return;
             }
             Tween_C.OnPunch_Func(transform);
-            IngameDataManager.Instance.UpdateMoney(EMoneyType.Gold, -REROOL_COST);
+            IngameDataManager.Instance.UpdateMoney(EMoneyType.Gold, -mRerollCost);
+            if (GameManager.Instance.CurrentStageData.StageType == StageType.Survive)
+            {
+                mRerollCost += 10;
+            }
             for (int i = 0; i < mSummonButtons.Length; ++i)
             {
                 mSummonButtons[i].Reroll();
